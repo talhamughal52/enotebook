7acbdfe73701aa45d3296b579d5782a1bba5eab3
talhamughal52 <talhamughal52352@gmail.com>
2023-04-24 04:19:42 +0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const mongoURI =
+  process.env.mongoURI ||
+  "mongodb://localhost:27017/enotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+
 const connectToMongo = () => {
-  mongoose.connect(process.env.mongoURI);
+  mongoose.connect(mongoURI);
   // mongoose.connect(process.env.mongoURI, () => {
   //   console.log("Connected to Mongo Successfully");
   // });
