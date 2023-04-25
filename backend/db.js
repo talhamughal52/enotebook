1653d977ca4b95bd97f010c7d37b1f2b22292be9
talhamughal52 <talhamughal52352@gmail.com>
2023-04-26 02:35:04 +0500

    Revert "update"
    This reverts commit e99b9c0edf57e689cc1fd1fba3a6f12759f1fb0e.

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// const mongoURI =
-//   process.env.mongoURI ||
-//   "mongodb://localhost:27017/enotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 const mongoURI =
   process.env.mongoURI ||
-  "mongodb+srv://talha_mughal52:fNtpeVzZIJOQskkD@enotebook.g1lncml.mongodb.net/enotebook?retryWrites=true&w=majority";
+  "mongodb://localhost:27017/enotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI);
